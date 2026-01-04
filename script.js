3ab5ed0ae1ae4e139b3acfcf18293d3cc96bb19e
nikhilkaraggi <nikhilkaraggi.devops@gmail.com>
2026-01-04 17:03:53 +0530

    Change alert message in contact form submission

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 // Form submission handling
 document.getElementById('contact-form').addEventListener('submit', function (e) {
   e.preventDefault();
-  alert('Hello Nikhil! have a good day thankyou.');
+  alert('Welcome Nikhil');
   this.reset();
 });
